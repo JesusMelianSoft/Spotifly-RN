360f35d9d10b978689568d3eca19deca604fb81d
JesusMelianSoft <95422521+JesusMelianSoft@users.noreply.github.com>
2022-05-24 17:14:29 +0100

    Revert "Creting nav bar"

--- PlayList/testFilter.js
+++
@@ -1,9 +0,0 @@
-const list = ["hello", "keep me alive", "adios"]
-const TEXT = "keep";
-const filter = (TEXT) => {
-    const myFilter = list.filter(function (str) {return str.includes(TEXT); })
-    console.log("HOLAA")
-}
-
-const myFilter = list.filter(function (str) {return str.includes(TEXT); })
-    console.log("HOLAA")
